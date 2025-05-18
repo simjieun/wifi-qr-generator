ee5ca2475743bb75ce52caebc6d81e1f59c9f6c4
simjieun <simjieun@encar.com>
2025-05-19 08:51:03 +0900

    홈페이지 버튼 텍스트를 'hello'에서 'Hello world'로 변경하고, 새로운 WiFi QR 생성기 페이지 추가

--- src/app/wifi/page.tsx
+++ src/app/wifi/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Button>Hello world</Button>
+      <h1 className="text-4xl font-bold mb-6">Hello</h1>
+      <Button>WiFi QR Generator</Button>
     </main>
   );
-}
+} 
